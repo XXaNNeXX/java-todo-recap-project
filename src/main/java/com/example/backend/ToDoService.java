8358576036c583773a9d92227953ba682e5b29e9
neuefische <info@neuefische.de>
2023-09-22 11:17:47 +0200

    fix updateToDo method

--- src/main/java/com/example/backend/ToDoService.java
+++ src/main/java/com/example/backend/ToDoService.java
@@ -1,18 +1,17 @@
 package com.example.backend;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@AllArgsConstructor
 public class ToDoService {
 
     private ToDoRepo toDoRepo;
 
-    public ToDoService(ToDoRepo toDoRepo) {
-        this.toDoRepo = toDoRepo;
-    }
 
     public List<ToDo> findAllToDos() {
         return toDoRepo.findAll();
@@ -26,7 +25,10 @@ public class ToDoService {
         return toDoRepo.save(toDo);
     }
 
-    public ToDo updateToDo(ToDo toDo) {
+    public ToDo updateToDo(String id, ToDo toDo) {
+        if(!id.equals(toDo.id())) {
+            throw new IllegalArgumentException("Das ToDo existiert nicht");
+        }
         return toDoRepo.save(toDo);
     }
 
