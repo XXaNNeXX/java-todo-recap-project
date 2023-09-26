cbb9a1347addf12e5c5fc598c018e4f54dddb601
neuefische <info@neuefische.de>
2023-09-26 19:13:18 +0200

    add addToDo integration test method

--- src/test/java/com/example/backend/ToDoIntegrationTest.java
+++ src/test/java/com/example/backend/ToDoIntegrationTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -45,6 +46,29 @@ public class ToDoIntegrationTest {
         toDoRepo.save(new ToDo("1", "gassi gehen", "OPEN"));
 
         mockMvc.perform(MockMvcRequestBuilders.get("/api/todo/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                        {
+                            "id": "1",
+                            "description": "gassi gehen",
+                            "status": "OPEN"
+                        }
+                        """));
+    }
+
+    @Test
+    @DirtiesContext
+    void addToDoTest() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/todo")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                        {
+                            "id": "1",
+                            "description": "gassi gehen",
+                            "status": "OPEN"
+                        }
+                        """))
                 .andExpect(status().isOk())
                 .andExpect(content().json("""
                         {
